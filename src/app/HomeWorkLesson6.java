951a1d05887b0cbf013687ba7c44e3f7e63319a4
Dash19haPol <polovnevadd@gmail.com>
2024-10-08 17:58:56 +0300

    HomeWorkLesson6

---
+++ src/app/HomeWorkLesson6.java
@@ -0,0 +1,13 @@
+package app;
+
+public class HomeWorkLesson6 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 6; i++) {
+            sum = sum + i;
+            System.out.println(i + ") Num is " + i + ", sum is " + sum);
+        }
+        System.out.println("______________________");
+        System.out.println("Sum of numbers is " + sum);
+    }
+}
